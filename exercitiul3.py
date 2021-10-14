7379af3f4ec9da47363517f104c3e86d2e2131b1
AndreiApopii <70907129+AndreiApopii@users.noreply.github.com>
2021-10-14 17:55:44 +0300

    Add files via upload

---
+++ exercitiul3.py
@@ -0,0 +1,4 @@
+np=input('ce nume si prenume ai')
+profesia=input('ce iti doresti sa devii in viitor')
+print=('ma numesc',np)
+print=('doresc sa devin',profesia)
---
+++ problema8.py
@@ -0,0 +1,15 @@
+x='A B C D E F G H I J K L M N O P Q R S T U V W X Y Z'
+for i in range(0,len(x)) :
+    a=x.replace('Z','A')
+print('b)',a)
+
+y='A B C D E F G H I J K L M N O P Q R S T U V W X Y Z'
+for i in range(0,len(y)) :
+    b=y.replace(' ','-')
+print('c)',b)
+ 
+z='A B C D E F G H I J K L M N O P Q R S T U V W X Y Z'
+for i in range(0,len(z)) :
+     c=z.replace('A','len(z+1)')
+print('a)',c)
+
